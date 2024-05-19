cbc97f894eee93b21a519169614c3aea584faf26
XTu17As <richard91.us@gmail.com>
2024-05-20 00:28:20 +0700

    Fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laundry App</title>
+        <title>Laundry Apps</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
